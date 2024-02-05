82ba68d22f4c564d09e6babed8876f9964471ce7
Olesia_Is <isichenko.ok@gmail.com>
2024-02-05 18:12:18 +0300

    Добавит проверку, были ли переданы данные для рендера миниатюр

--- js/thumb.js
+++ js/thumb.js
@@ -2,15 +2,17 @@ const thumbTemplate = document.querySelector('#picture').content.querySelector('
 const thumbsContainer = document.querySelector('.pictures');
 
 const renderThumbs = (photoData) => {
-  const thumbsFragment = document.createDocumentFragment();
-  photoData.forEach((photo) => {
-    const thumb = thumbTemplate.cloneNode(true);
-    thumb.querySelector('.picture__img').src = photo.url;
-    thumb.querySelector('.picture__likes').textContent = photo.likes;
-    thumb.querySelector('.picture__comments').textContent = photo.comments.length;
-    thumbsFragment.append(thumb);
-  });
-  thumbsContainer.append(thumbsFragment);
+  if (photoData) {
+    const thumbsFragment = document.createDocumentFragment();
+    photoData.forEach((photo) => {
+      const thumb = thumbTemplate.cloneNode(true);
+      thumb.querySelector('.picture__img').src = photo.url;
+      thumb.querySelector('.picture__likes').textContent = photo.likes;
+      thumb.querySelector('.picture__comments').textContent = photo.comments.length;
+      thumbsFragment.append(thumb);
+    });
+    thumbsContainer.append(thumbsFragment);
+  }
 };
 
 export {renderThumbs};
