826db34504d525699dd5d0c7440e67041e3ba8bf
Olesia_Is <isichenko.ok@gmail.com>
2024-02-12 13:03:22 +0300

    Добавит правки в первый вариант кода для отображения комментариев. Добавит еще 2 варианта кода для отображения комментариев

--- js/full-photo.js
+++ js/full-photo.js
@@ -5,8 +5,8 @@ const fullPhoto = document.querySelector('.big-picture');
 const fullPhotoImage = fullPhoto.querySelector('.big-picture__img > img');
 const photoDescription = fullPhoto.querySelector('.social__caption');
 const likesCount = fullPhoto.querySelector('.likes-count');
-const fullCommentsCount = fullPhoto.querySelector('.social__comment-count');
-const commentsQuantity = fullCommentsCount.querySelector('.comments-count');
+const commentsCurrentQuantity = fullPhoto.querySelector('.comments-current-count');
+const commentsQuantity = fullPhoto.querySelector('.comments-count');
 const commentsList = fullPhoto.querySelector('.social__comments');
 const commentsLoader = fullPhoto.querySelector('.comments-loader');
 const closedButton = fullPhoto.querySelector('.big-picture__cancel');
@@ -24,38 +24,6 @@ const createComment = (commentInfo) => {
   commentText.append(commentInfo.message);
   comment.append(commentAuthor, commentText);
   return comment;
-};
-
-const getCommentsPack = (commentsData, startIndex) => {
-  if (commentsData) {
-    const fragment = document.createDocumentFragment();
-    commentsData.slice(startIndex, startIndex + COMMENTS_PER_TIME).forEach((commentInfo) => {
-      fragment.append(createComment(commentInfo));
-    });
-    return fragment;
-  }
-  return null;
-};
-
-const addCommentsPack = (commentsData) => {
-  if (commentsData.length > 0) {
-    commentsList.append(getCommentsPack(commentsData, commentsList.children.length));
-  }
-
-  fullCommentsCount.childNodes[0].textContent = `${commentsList.children.length} из `;
-
-  if (commentsList.children.length === commentsData.length) {
-    commentsLoader.classList.add('hidden');
-  } else {
-    commentsLoader.classList.remove('hidden');
-  }
-};
-
-const getCommentsLoaderHandler = (commentsData) => {
-  onCommentsLoaderClick = () => {
-    addCommentsPack(commentsData);
-  };
-  return onCommentsLoaderClick;
 };
 
 const closeFullPhotoModal = () => {
@@ -76,6 +44,41 @@ const onClosedButtonClick = () => {
   closeFullPhotoModal();
 };
 
+
+// Первый вариант
+
+const getCommentsPack = (commentsData, startIndex) => {
+  if (commentsData) {
+    const fragment = document.createDocumentFragment();
+    commentsData.slice(startIndex, startIndex + COMMENTS_PER_TIME).forEach((commentInfo) => {
+      fragment.append(createComment(commentInfo));
+    });
+    return fragment;
+  }
+  return null;
+};
+
+const addCommentsPack = (commentsData) => {
+  if (commentsData) {
+    commentsList.append(getCommentsPack(commentsData, commentsList.children.length));
+  }
+
+  commentsCurrentQuantity.textContent = commentsList.children.length;
+
+  if (commentsList.children.length === commentsData.length) {
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+};
+
+const getCommentsLoaderHandler = (commentsData) => {
+  onCommentsLoaderClick = () => {
+    addCommentsPack(commentsData);
+  };
+  return onCommentsLoaderClick;
+};
+
 const openFullPhotoModal = ({url, likes = 0, comments = [], description = ''}) => {
   if (url) {
     fullPhotoImage.src = url;
@@ -93,6 +96,135 @@ const openFullPhotoModal = ({url, likes = 0, comments = [], description = ''}) =
     fullPhoto.classList.remove('hidden');
   }
 };
+
+
+// Второй вариант
+// альтернативный код с отображением комментов, где ты хранишь текущее отображение комментов и сколько есть у определенного поста
+
+// let renderedCommentsQuantity = 0;
+
+// const getCommentsPack = (commentsData, startIndex = renderedCommentsQuantity) => {
+//   if (commentsData) {
+//     const fragment = document.createDocumentFragment();
+//     commentsData
+//       .slice(startIndex, startIndex + COMMENTS_PER_TIME)
+//       .forEach((commentInfo) => {
+//         fragment.append(createComment(commentInfo));
+//         renderedCommentsQuantity++;
+//       });
+//     return fragment;
+//   }
+//   return null;
+// };
+
+// const addCommentsPack = (commentsData) => {
+//   if (commentsData) {
+//     commentsList.append(getCommentsPack(commentsData));
+//   }
+
+//   commentsCurrentQuantity.textContent = renderedCommentsQuantity;
+
+//   if (renderedCommentsQuantity === commentsData.length) {
+//     commentsLoader.classList.add('hidden');
+//   }
+// };
+
+// const getCommentsLoaderHandler = (commentsData) => {
+//   onCommentsLoaderClick = () => {
+//     addCommentsPack(commentsData);
+//   };
+//   return onCommentsLoaderClick;
+// };
+
+// const openFullPhotoModal = ({url, likes = 0, comments = [], description = ''}) => {
+//   if (url) {
+//     fullPhotoImage.src = url;
+//     fullPhotoImage.alt = '';
+//     photoDescription.textContent = description;
+//     likesCount.textContent = likes;
+//     commentsQuantity.textContent = comments.length;
+//     commentsList.innerHTML = '';
+
+//     renderedCommentsQuantity = 0;
+//     commentsLoader.classList.remove('hidden');
+
+//     addCommentsPack(comments);
+
+//     commentsLoader.addEventListener('click', getCommentsLoaderHandler(comments));
+//     document.body.classList.add('modal-open');
+//     document.addEventListener('keydown', onEscKeydown, {once: true});
+//     fullPhoto.classList.remove('hidden');
+//   }
+// };
+
+
+// Третий вариант
+// в нем мы в разметку добавляем сразу все комменты, а потом при клике отображаем нужное кол-во
+
+// let visibleCommentsQuantity = 0;
+
+// const getComments = (commentsData) => {
+//   if (commentsData) {
+//     const fragment = document.createDocumentFragment();
+//     commentsData.forEach((commentInfo) => {
+//       const commentElement = createComment(commentInfo);
+//       commentElement.classList.add('hidden');
+//       fragment.append(commentElement);
+//     });
+//     return fragment;
+//   }
+// };
+
+// const showComments = (commentElements, startIndex = visibleCommentsQuantity) => {
+//   if (commentElements) {
+//     for (let i = startIndex; i < startIndex + COMMENTS_PER_TIME; i++) {
+//       if (commentElements[i]) {
+//         const commentElement = commentElements[i];
+//         commentElement.classList.remove('hidden');
+//         visibleCommentsQuantity++;
+//       } else {
+//         break;
+//       }
+//     }
+
+//     commentsCurrentQuantity.textContent = visibleCommentsQuantity;
+
+//     if (visibleCommentsQuantity === commentElements.length) {
+//       commentsLoader.classList.add('hidden');
+//     }
+//   }
+// };
+
+// const getCommentsLoaderHandler = (commentElements) => {
+//   onCommentsLoaderClick = () => {
+//     showComments(commentElements);
+//   };
+//   return onCommentsLoaderClick;
+// };
+
+// const openFullPhotoModal = ({url, likes = 0, comments = [], description = ''}) => {
+//   if (url) {
+//     fullPhotoImage.src = url;
+//     fullPhotoImage.alt = '';
+//     photoDescription.textContent = description;
+//     likesCount.textContent = likes;
+//     commentsQuantity.textContent = comments.length;
+//     commentsList.innerHTML = '';
+
+//     visibleCommentsQuantity = 0;
+//     commentsLoader.classList.remove('hidden');
+
+//     commentsList.append(getComments(comments));
+//     const commentElements = commentsList.querySelectorAll('.social__comment');
+//     showComments(commentElements);
+
+//     commentsLoader.addEventListener('click', getCommentsLoaderHandler(commentElements));
+//     document.body.classList.add('modal-open');
+//     document.addEventListener('keydown', onEscKeydown, {once: true});
+//     fullPhoto.classList.remove('hidden');
+//   }
+// };
+
 
 closedButton.addEventListener('click', onClosedButtonClick);
 
